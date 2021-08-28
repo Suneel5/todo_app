68ad9cfaede2b9236042459ddf97e24b205066bf
suneel <suneelpanday@gmail.com>
2021-08-28 13:48:38 +0545

    'wauuuuu'

--- todo/models.py
+++ todo/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 
 class task(models.Model):
     title=models.CharField(max_length=50)
+    completed=models.BooleanField(default=False)
+    date_created=models.DateTimeField(default=datetime.now())
+
+
     
     
