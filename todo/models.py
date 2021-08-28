a1ed9e9febb76d50e4c5fdecd43d17f121bf6ac7
suneel <suneelpanday@gmail.com>
2021-08-28 15:51:56 +0545

    working well

--- todo/models.py
+++ todo/models.py
@@ -1,12 +1,11 @@
 from django.db import models
-from datetime import datetime
-
+from django.utils import timezone
 # Create your models here.
 
 class task(models.Model):
     title=models.CharField(max_length=50)
     completed=models.BooleanField(default=False)
-    date_created=models.DateTimeField(default=datetime.now())
+    date_created=models.DateTimeField(default=timezone.now())
 
 
     
