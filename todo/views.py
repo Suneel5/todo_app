179951b854c2dc1009d95e799045802654068b60
suneel <suneelpanday@gmail.com>
2021-08-28 13:48:20 +0545

    'waauu'

--- todo/views.py
+++ todo/views.py
@@ -1,23 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse,HttpResponseRedirect
 from .forms import todoform
 from .models import task
-
 # Create your views here.
 
 def home(request):
-    # titles =['learn this','learn that','dont waste times']
     taskes=task.objects.all()
     if request.method == 'POST':
         todo_form=todoform(request.POST)
         if todo_form.is_valid():
-            #todo_form.save()
-            return HttpResponse('todo added')
+            todo_form.save()
+            return redirect('home')
     else:
         todo_form=todoform()
 
-    # form=todoform()
-    #print(form)  
+
 
     context={'taskes': taskes,'form': todo_form}
 
